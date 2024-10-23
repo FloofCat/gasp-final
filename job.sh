48e4e6a06dcac11cd8512a3bd7cb21abc20deabd
BitC3t <bitc3t@gmail.com>
2024-10-23 19:21:10 +0530

    sim2

--- job.sh
+++ job.sh
@@ -4,4 +4,4 @@
 #SBATCH --gres=gpu:A100:3
 #SBATCH --partition=gpu
 
-srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-final/run.py --task=train
+srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-final-copy/run.py --task=train
