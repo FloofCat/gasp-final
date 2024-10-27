21a58fd8f93f1a2da2a23ce21cc91839da30537f
BitC3t <bitc3t@gmail.com>
2024-10-28 04:49:32 +0530

    mistral

--- job.sh
+++ job.sh
@@ -4,4 +4,4 @@
 #SBATCH --gres=gpu:A100:3
 #SBATCH --partition=gpu
 
-srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-falcon/run.py --task=all
+srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-mistral/run.py --task=all
