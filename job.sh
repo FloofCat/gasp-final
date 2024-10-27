bef9174a1377ac8757164297072602cfa8b11d94
BitC3t <bitc3t@gmail.com>
2024-10-27 19:16:25 +0530

    ehm

--- job.sh
+++ job.sh
@@ -5,5 +5,3 @@
 #SBATCH --partition=gpu
 
 srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-mistral/run.py --task=all
-
-foo='falcon' && mkdir gasp-$foo && cd gasp-$foo && git init 
