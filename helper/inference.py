a63dff976f5629d8599d99062a1703f351625029
BitC3t <bitc3t@gmail.com>
2024-10-28 17:00:28 +0530

    llama3.1

--- helper/inference.py
+++ helper/inference.py
@@ -92,6 +92,6 @@ class Inference:
         df['chosen'] = self.chosen
         df['rejected'] = self.rejected
         
-        df.to_csv("./gasp-llama3/logs/inference.csv", index=False)
+        df.to_csv("./gasp-llama3-1/logs/inference.csv", index=False)
                     
                     
