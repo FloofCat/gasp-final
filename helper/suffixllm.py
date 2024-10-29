641f347985f0799a0f00188cdd69d7c7f99eb4fb
BitC3t <bitc3t@gmail.com>
2024-10-29 08:12:22 +0530

    mew

--- helper/suffixllm.py
+++ helper/suffixllm.py
@@ -69,7 +69,7 @@ class SuffixLLM:
     
     def check_if_trained(self):
         # Check if model_name + "_finetuned" exists in the models directory
-        if not os.path.exists(f"./gasp-mistral/models/{self.model_name}_finetuned"):
+        if not os.path.exists(f"./gasp-llama2/models/{self.model_name}_finetuned"):
             return False
         return True
     
@@ -128,8 +128,8 @@ class SuffixLLM:
         print("[SUFFIX-LLM] Training Started")
 
         self.trainer.train()
-        self.model.save_pretrained(f"./gasp-mistral/models/{self.model_name}_finetuned")
-        self.tokenizer.save_pretrained(f"./gasp-mistral/models/{self.model_name}_finetuned")
+        self.model.save_pretrained(f"./gasp-llama2/models/{self.model_name}_finetuned")
+        self.tokenizer.save_pretrained(f"./gasp-llama2/models/{self.model_name}_finetuned")
         print("[SUFFIX-LLM] Training Completed")
 
     def setup_inference(self):
@@ -141,12 +141,12 @@ class SuffixLLM:
 
         self.load_dataset()
 
-        self.model = AutoModelForCausalLM.from_pretrained(f"./gasp-mistral/models/{self.model_name}_finetuned",
+        self.model = AutoModelForCausalLM.from_pretrained(f"./gasp-llama2/models/{self.model_name}_finetuned",
                                                             torch_dtype=torch.float16,
                                                             trust_remote_code=True,
                                                             device_map="auto")
         # .to(self.device) -- Seems like ORPO hates this. OOM at all times.
-        self.tokenizer = AutoTokenizer.from_pretrained(f"./gasp-mistral/models/{self.model_name}_finetuned",
+        self.tokenizer = AutoTokenizer.from_pretrained(f"./gasp-llama2/models/{self.model_name}_finetuned",
                                                         trust_remote_code=True,
                                                         use_fast=False)
         self.tokenizer.pad_token = self.tokenizer.eos_token
@@ -158,11 +158,11 @@ class SuffixLLM:
             del self.tokenizer
             torch.cuda.empty_cache()
         
-        self.model = AutoModelForCausalLM.from_pretrained(f"./gasp-mistral/models/{blackbox_name}_orpo",
+        self.model = AutoModelForCausalLM.from_pretrained(f"./gasp-llama2/models/{blackbox_name}_orpo",
                                                             torch_dtype=torch.float16,
                                                             trust_remote_code=True,
                                                             device_map="auto")
-        self.tokenizer = AutoTokenizer.from_pretrained(f"./gasp-mistral/models/{blackbox_name}_orpo",
+        self.tokenizer = AutoTokenizer.from_pretrained(f"./gasp-llama2/models/{blackbox_name}_orpo",
                                                         trust_remote_code=True,
                                                         use_fast=False)
         
