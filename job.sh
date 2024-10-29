92e8e772e4c346f6c243e3aa854bbbefb8b2abb3
BitC3t <bitc3t@gmail.com>
2024-10-29 22:07:19 +0530

    llama3

--- job.sh
+++ job.sh
@@ -4,4 +4,4 @@
 #SBATCH --gres=gpu:A100:3
 #SBATCH --partition=gpu
 
-srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-llama3/run.py --task=eval
+srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-llama2/run.py --task=eval
