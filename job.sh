7668e07ffd33d8c39141015ec8fbdad9f1470c86
BitC3t <bitc3t@gmail.com>
2024-10-31 01:20:39 +0530

    .

--- job.sh
+++ job.sh
@@ -3,5 +3,6 @@
 #SBATCH --job-name=gasp_job
 #SBATCH --gres=gpu:A100:3
 #SBATCH --partition=gpu
+#SBATCH --time 48:00:00
 
 srun --container-image=floofcat/pvt_advattacks:v5 --container-mounts=$HOME/CISPA-az6/adv_attacks_llm-2024/baseline:/root python3 $HOME/CISPA-az6/adv_attacks_llm-2024/baseline/gasp-falcon/run.py --task=all
